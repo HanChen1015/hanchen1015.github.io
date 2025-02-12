04f40171b8c38dca2222e2f62f087e79779a7854
HanChen1015 <Hanguangchen1015@gmail.com>
2025-02-12 15:19:42 -0800

    Deleted unnecessary js code

--- animations.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.contacts').onclick = () => {
-        window.scrollTo(0, document.body.scrollHeight);
-    }
-});
